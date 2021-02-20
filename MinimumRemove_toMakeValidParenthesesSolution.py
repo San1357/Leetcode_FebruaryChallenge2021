7ad0f3904ab765a81e17983a63cb16822f320899
San Ayush <41052723+San1357@users.noreply.github.com>
2021-02-20 12:22:00 +0530

    Create MinimumRemove_toMakeValidParenthesesSolution.py

---
+++ MinimumRemove_toMakeValidParenthesesSolution.py
@@ -0,0 +1,19 @@
+#problem :
+
+#CODE:
+
+class Solution:
+    def minRemoveToMakeValid(self, S: str) -> str:
+        S, stack = list(S), []
+        for i , c in enumerate(S):
+            if c == ")":
+                if stack:
+                    stack.pop()
+                else :
+                    S[i] = ""
+            elif c =="(": stack.append(i)
+        for i in stack:
+            S[i] = ""
+        return "".join(S)
+            
+        
